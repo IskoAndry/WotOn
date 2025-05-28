adc0e95c2776221a5e5c911c511a2274cfab62db
isakov <isakov4852@mail.ru>
2025-05-28 23:58:09 +0300

    commit prettierrc

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { variableWotOn, ideas } from './data.ts'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <h1>{variableWotOn.titleApp}</h1>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {ideas.map((idea) => {
+          return (
+            <div key={idea.nick}>
+              <h2>{idea.nametitle}</h2>
+              <p>{idea.description}</p>
+            </div>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
